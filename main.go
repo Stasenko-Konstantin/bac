4ac079c323e16d2058d760d8db3316bda89757ad
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2025-02-07 23:55:06 +0500

    add rules

--- main.go
+++ main.go
@@ -16,40 +16,25 @@ import (
 )
 
 var (
-	players           int
-	currPlayer        = 1
-	regime            string
 	mysteryWord       string
 	mysteryWordLength int
-)
-
-const (
-	singleGame  = "одиночная"
-	localGame   = "локальная"
-	networkGame = "сетевая"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("bac")
 
-	//choosePlayers := widget.NewSelect(
-	//	[]string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"},
-	//	func(p string) {
-	//		i, _ := strconv.Atoi(p)
-	//		players = i
-	//	})
-	//choosePlayers.SetSelected("2")
-
 	var (
 		newGameB            *widget.Button
+		newGameC            *fyne.Container
+		rulesL              = widget.NewLabel(rules)
 		startGameC          *fyne.Container
-		currPlayerL         = widget.NewLabel("")
 		mysteryWordsLengthL = widget.NewLabel("")
 		newWordE            = widget.NewEntry()
-		givenWords          []string
+		givenWords          = map[string]struct{}{}
 		givenWordsL         = widget.NewLabel("")
 		okB                 = widget.NewButton("ok", func() {})
+		againB              = widget.NewButton("заново", func() {})
 	)
 
 	_ = givenWords
@@ -59,15 +44,11 @@ func main() {
 			newWordE.SetText("")
 		}()
 
-		if regime != singleGame {
-			currPlayerL.SetText(fmt.Sprintf("текущий игрок: %d", currPlayer))
-		}
-
 		word := newWordE.Text
 		wordLength := utf8.RuneCountInString(word)
 		if wordLength != mysteryWordLength {
 			text := fmt.Sprintf(
-				"длина введенного слова\nне соответствует длине отгадываемого:\n%d и %d соответственно",
+				"длина введенного слова\nне соответствует длине отгадываемого:\n%d и %d соответственно.",
 				wordLength,
 				mysteryWordLength,
 			)
@@ -75,7 +56,7 @@ func main() {
 				text,
 				"да",
 				"тоже да",
-				widget.NewLabel("я буду внимательней"),
+				widget.NewLabel("я буду внимательней!"),
 				func(b bool) {
 					newGameB.SetText("")
 				},
@@ -106,74 +87,60 @@ func main() {
 					}
 				}
 			}
-			givenWordsL.SetText(fmt.Sprintf("%s - %dб, %dк\n%s", word, bulls, cows, givenWordsL.Text))
+			if _, ok := givenWords[word]; !ok {
+				givenWordsL.SetText(fmt.Sprintf("%s - %dб, %dк\n%s", word, bulls, cows, givenWordsL.Text))
+			}
+			givenWords[word] = struct{}{}
 		}
 
 		if word == mysteryWord {
-			text := "победа!"
-			if regime != singleGame {
-				text = "" // TODO
-			}
+			text := "ты угадал!"
 			dialog.ShowCustomConfirm("победа!", "еще!", "хватит...", widget.NewLabel(text), func(b bool) {
 				if !b {
 					os.Exit(1)
 				}
-				currPlayerL.SetText("")
 				mysteryWordsLengthL.SetText("")
 				givenWordsL.SetText("")
 				startGameC.Hide()
-				newGameB.Show()
+				newGameC.Show()
 			}, w)
 			return
 		}
-
-		if regime != singleGame {
-			if currPlayer == players {
-				currPlayer = 1
-			} else {
-				currPlayer++
-			}
-		}
 	}
 
-	chooseRegime := widget.NewRadioGroup([]string{singleGame, localGame, networkGame}, func(p string) {
-		regime = p
-	})
+	againB.OnTapped = func() {
+		mysteryWordsLengthL.SetText("")
+		givenWordsL.SetText("")
+		startGameC.Hide()
+		newGameC.Show()
+	}
 
 	newGameB = widget.NewButton("Новая игра", func() {
-		dialog.ShowCustomConfirm("Выберите режим игры:", "ok", "не хочу", chooseRegime, func(b bool) {
-			if !b {
-				startGameC.Hide()
-				newGameB.Show()
-			}
+		chooseWordLength := widget.NewSelect(
+			[]string{"3", "4", "5", "6", "7", "8", "9", "10", "11", "12"},
+			func(wl string) {
+				i, _ := strconv.Atoi(wl)
+				words := dict[i]
+				mysteryWord = words[rand.Intn(len(words))]
+				mysteryWordLength = utf8.RuneCountInString(mysteryWord)
+				mysteryWordsLengthL.SetText(fmt.Sprintf("длина отгадываемого слова: %d", mysteryWordLength))
+				fmt.Println(mysteryWord)
+			})
+		chooseWordLength.SetSelected("3")
+		dialog.ShowCustom("Выберите длинну угадываемого слова", "ok", chooseWordLength, w)
 
-			if regime == singleGame {
-				chooseWordLength := widget.NewSelect(
-					[]string{"3", "4", "5", "6", "7", "8", "9", "10", "11", "12"},
-					func(wl string) {
-						i, _ := strconv.Atoi(wl)
-						words := dict[i]
-						mysteryWord = words[rand.Intn(len(words))]
-						mysteryWordLength = utf8.RuneCountInString(mysteryWord)
-						mysteryWordsLengthL.SetText(fmt.Sprintf("длина отгадываемого слова: %d", mysteryWordLength))
-						currPlayerL.SetText(fmt.Sprintf("текущий игрок: %d", currPlayer))
-						if regime == singleGame {
-							currPlayerL.SetText("")
-						}
-						fmt.Println(mysteryWord)
-					})
-				chooseWordLength.SetSelected("3")
-				dialog.ShowCustom("Выберите длинну угадываемого слова", "ok", chooseWordLength, w)
-			}
-		}, w)
-		newGameB.Hide()
+		newGameC.Hide()
 		startGameC.Show()
-		currPlayer = 1
 	})
+
+	newGameC = container.NewVBox(
+		newGameB,
+		rulesL,
+	)
 
 	startGameC = container.NewGridWithRows(2,
 		container.NewVBox(
-			currPlayerL,
+			againB,
 			mysteryWordsLengthL,
 			newWordE,
 			okB,
@@ -184,8 +151,7 @@ func main() {
 	startGameC.Hide()
 
 	content := container.NewVBox(
-		widget.NewLabel("\n\n\n\n"),
-		newGameB,
+		newGameC,
 		startGameC,
 	)
 
